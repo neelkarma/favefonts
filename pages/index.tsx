e93a97c2cfcb4a4552cc07b4e08b4294b2e7b6c8
chickensalt <nsdj.sharma@gmail.com>
2021-12-20 17:43:20 +1100

    add title tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,15 @@ import { TextInput } from "components/TextInput";
 import { useState } from "react";
 import type { NextPage } from "next";
 import { FontPreview } from "components/FontPreview";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const [text, setText] = useState("");
   return (
     <div className="container max-w-[90%] md:max-w-[70%] lg:max-w-[50%] m-auto pt-5 md:pt-10">
+      <Head>
+        <title>Fave Fonts</title>
+      </Head>
       <h1 className="font-heading font-bold">Fave Fonts</h1>
       <h3 className="font-serif font-semibold italic text-gray-600 dark:text-gray-400">
         A showcase of some of my favourite fonts.
